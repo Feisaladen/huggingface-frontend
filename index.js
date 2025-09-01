df657ff2caea1edfad87ebeb7a11e9087a21faab
feisal.suheil <Feisaladen32@gmail.com>
2025-09-01 16:14:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const questionInput = document.getElementById("question");
 const askBtn = document.getElementById("askBtn");
 
 // Deployed backend URL
-const API_BASE = "https://huggingface-backend-3.onrender.com";
+const API_BASE = "https://huggingface-backend-4.onrender.com";
 
 // List of banned words for kid-safe filtering
 const bannedWords = [
@@ -69,4 +69,5 @@ askBtn.addEventListener("click", askQuestion);
 questionInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") askQuestion();
 });
+
 
